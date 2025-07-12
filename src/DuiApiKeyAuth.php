45791900dfae83a31747c25d0253a9a88bb58953
Dmytro Mytrofanov <dmitrof.development@gmail.com>
2025-07-12 19:53:19 +0300

    refactoring

--- src/DuiApiKeyAuth.php
+++ src/DuiApiKeyAuth.php
@@ -147,7 +147,12 @@ class DuiApiKeyAuth extends BaseHttpBearerAuth
         $model->api_key = null;
         $model->new_api_key = null;
         $model->public_key = null;
-        $model->private_key = null;
+        if ($model->hasAttribute('private_key')) {
+            $model->private_key = null;
+        }
+        if ($model->hasAttribute('new_private_key')) {
+            $model->new_private_key = null;
+        }
 
         return $model;
     }
